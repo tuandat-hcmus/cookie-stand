482cf5bd4335166a40ae34e33137ffad71576718
tuandat-hcmus <ngotuandat.lop9a@gmail.com>
2023-08-01 03:08:25 +0000

    update sales.html

--- js/app.js
+++ js/app.js
@@ -6,28 +6,67 @@ function Location(name, min, max, avg) {
     this.max = max;
     this.avg = avg;
     this.cookiesPerHour = [];
-    this.generate = function() {
-        if(this.cookiesPerHour.length > 0){
+    this.generate = function () {
+        if (this.cookiesPerHour.length > 0) {
             return;
         }
-        for(let i = 0; i < 12; i++){
-            let customersPerHour = Math.floor(Math.random() * 20 + 1); // random number from 1 to 21
+        for (let i = 0; i < 13; i++) {
+            let customersPerHour = Math.floor(Math.random() * (this.max - this.min + 1) + this.min); // random number from min to max
             this.cookiesPerHour.push(Math.floor(customersPerHour * this.avg));
         }
     }
-    this.render = function() {
+    this.render = function () {
         this.generate();
-        const ul = document.createElement('ul');
-        for(let i = 0; i < 12; i++){
-            const li = document.createElement('li');
-            const liText = document.createTextNode(this.cookiesPerHour[i]);
-            li.appendChild(liText);
-            ul.appendChild(li);
+        const table = document.getElementById('table');
+        let sum = 0;
+        const tableRow = document.createElement('tr');
+        for (let i = -1; i < 14; i++) {
+            const tableData = document.createElement('td');
+            if (i < 0) {
+                tableData.innerText = this.name;
+            }
+            else if (i == 13) {
+                tableData.innerText = sum;
+            }
+            else {
+                sum += this.cookiesPerHour[i];
+                tableData.innerText = this.cookiesPerHour[i];
+            }
+            tableRow.appendChild(tableData);
         }
-        document.getElementById('main').appendChild(ul);
+        table.appendChild(tableRow);
     }
 }
 
+function createFooter(){
+    const table = document.getElementById('table');
+    const tableRow = document.createElement('tr');
+    const rowName = document.createElement('th');
+    rowName.innerText = 'Totals';
+    tableRow.appendChild(rowName);
+    //tableRow.setAttribute('id', 'tableFooter');
+    const rowList = document.querySelectorAll('tr');
+    for(let i = 1; i < 15; i++){
+        let sum = 0;
+        const footerData = document.createElement('th');
+        for(let j = 1; j < rowList.length; j++){
+            sum += parseInt(rowList[j].children[i].innerText);
+        }
+        footerData.innerText = sum;
+        tableRow.appendChild(footerData);
+    }
+    table.appendChild(tableRow);
+}
+
 const seattle = new Location('Seattle', 23, 65, 6.3);
+const tokyo = new Location('Tokyo', 3, 24, 1.2);
+const dubai = new Location('Dubai', 11, 38, 3.7);
+const paris = new Location('Paris', 20, 38, 2.3);
+const lima = new Location('Lima', 2, 16, 4.6);
 seattle.render();
+tokyo.render();
+dubai.render();
+paris.render();
+lima.render();
+createFooter();
 
